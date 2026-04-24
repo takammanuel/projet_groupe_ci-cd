a4be902d8e354705965b95cab895bcc7a6447563
CaroDtoc <olgadonkeng@gmail.com>
2026-04-24 14:53:53 +0200

    Update recettes interface and authentication fixes

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,9 +18,18 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
+        User::updateOrCreate([
             'email' => 'test@example.com',
+        ], [
+            'name' => 'Test User',
+            'password' => Hash::make('password'),
+        ]);
+
+        User::updateOrCreate([
+            'email' => 'test@test.com',
+        ], [
+            'name' => 'Test User 2',
+            'password' => Hash::make('password'),
         ]);
     }
 }
---
+++ backend/tmp_verify.php
@@ -0,0 +1,2 @@
+<?php
+var_dump(password_verify('password', '$2y$12$Lq/WydVwyWod.LX46CzDTOCCNiSkh3pT4kXLYoPfbU8sq7Lql.XEG'));
